c2622dd5fd970494c4a9704f8b3ffce536ddef4f
Raimon Esteve <raimon@nan-tic.com>
2017-11-13 11:49:29 +0100

    FIX search product name and allow inherit search value inherit task-032463

--- product.py
+++ product.py
@@ -15,6 +15,10 @@ class ProductMoves(Wizard):
     __name__ = 'product.moves'
     start = StateAction('stock.act_move_form')
 
+    def search_value_moves(self):
+        now = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+        return [('create_date', '>=', now)]
+
     def do_start(self, action):
         pool = Pool()
         Product = pool.get('product.product')
@@ -23,30 +27,24 @@ class ProductMoves(Wizard):
         context = Transaction().context
         prod_ids = context['active_ids']
 
-        product_ids = []
-        codes = []
+        codes = set()
         if context['active_model'] == 'product.template':
             for template in Template.search([('id', 'in', prod_ids)]):
                 for product in template.products:
-                    product_ids.append(product.id)
-                    codes.append(product.code or product.name)
+                    codes.add(product.code or product.name)
         else:
             for product in Product.search([('id', 'in', prod_ids)]):
-                product_ids.append(product.id)
-                codes.append(product.code or product.name)
+                codes.add(product.code or product.name)
 
         now = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
-        search_value = [
-            ('create_date', '>=', now),
-            ]
+        search_value = self.search_value_moves()
 
-        if len(product_ids) > 0:
-            domain = ['OR']
-            for prod_id in product_ids:
-                domain.append(('product', '=', prod_id))
+        domain = [('product', '=', code) for code in codes]
+        if len(codes) > 1:
+            domain.insert(0, 'OR')
             search_value += [domain]
         else:
-            search_value.append(('product', '=', product_ids[0]))
+            search_value += domain
 
         action['pyson_search_value'] = PYSONEncoder().encode(search_value)
 
